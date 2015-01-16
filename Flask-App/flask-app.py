8bea202d45693d25ab37607438565c3c53c84c6f
lrgeofflewis <LRGeoff@LRWorkComputer.local>
2015-01-16 14:19:32 +0000

    fiddling with it

--- Flask-App/flask-app.py
+++ Flask-App/flask-app.py
@@ -5,5 +5,9 @@ app = Flask (__name__)
 def hello():
   return 'Hello World!'
 
+@app.route('/bye')
+def the_end():
+  return 'The end of the world is nigh!'
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
